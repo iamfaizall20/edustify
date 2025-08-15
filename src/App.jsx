be132f28caa778569ee6acc0546a0c4403b62f64
Faizal Hassan <hassanfaizal047@gmail.com>
2025-08-15 12:06:00 +0500

    ----

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Title subtitle = 'What we Offer' title = 'Our Program' />
       <Programs />
     </div>
   )
